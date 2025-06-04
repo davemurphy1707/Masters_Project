9506d6b3b0f04e4b19373bef3f8bb8e7bcaf9428
David Murphy <davemurphy1707@gmail.com>
2025-06-04 11:59:36 +0100

    updates

--- testhmm1.py
+++ testhmm1.py
@@ -6,7 +6,7 @@ import itertools
 import argparse # module for command line inputs
 
 def main():
-
+    chr2_len = 242696752
     parser = argparse.ArgumentParser(description="Input a file and a a number of states, Hmm is trained.")
 
     parser.add_argument("input_file", type=str, help="Path to the input file")
@@ -23,33 +23,30 @@ def main():
     model = hmm.MultinomialHMM(n_components, n_iter = 1000,algorithm = "map")
     model.fit(X, lengths = [X.shape[0]])
 
+    print("\n--- Calculating Transition Probability Matrix ---")
     print(model.transmat_) 
+    print("\n--- Calculating Emission Probability Matrix ---")
     print(model.emissionprob_) 
+    print("\n--- Calculating Initial State Probability Matrix ---")
     print(model.startprob_) 
     
-    # --- Line added to calculate Stationary Distribution ---
+ 
     print("\n--- Calculating Stationary Distribution Vector ---")
     transition_matrix = model.transmat_
 
     # Calculate eigenvalues and eigenvectors of the transpose of the transition matrix
-    # The stationary distribution vector is the left eigenvector corresponding to eigenvalue 1
     eigenvalues, eigenvectors = np.linalg.eig(transition_matrix.T)
 
     # Find the eigenvector corresponding to the eigenvalue closest to 1.0
     # Use np.isclose for numerical stability, as eigenvalues might not be exactly 1.0
     stationary_vector = eigenvectors[:, np.isclose(eigenvalues, 1.0)][:, 0]
 
-    # Normalize the vector to sum to 1.0 (probabilities)
-    # The resulting vector might be complex if there are numerical issues, convert to real
+    # Normalize the vector to sum to 1.0 
     stationary_distribution = (stationary_vector / np.sum(stationary_vector)).real
 
     print("Stationary Distribution Vector:")
     print(stationary_distribution)
 
-    # You can verify if it's indeed stationary:
-    # print("\nVerification (pi * T - pi):")
-    # print(np.dot(stationary_distribution, transition_matrix) - stationary_distribution
-    
     aic = model.aic(X)
     print("aic")
     print(aic)
@@ -58,15 +55,33 @@ def main():
     print("bic")
     print(bic)
     
+    #Compute the posterior probability for each state in the model.
+    print("Posterior Probability for each state in the model")
+    posterior_probs = model.predict_proba(X)
+    print(posterior_probs)
+    
+    print("Converged:", model.monitor_.converged)
+
+    print("Iterations:", model.monitor_.iter)
+    
+    print("Final Log Likelihood:", model.monitor_.history[-1])
+    print("Log Likelihood History:", model.monitor_.history)
+    
     state = model.predict(X)
 
     data["state"] = state
     data["chrom"] = "chr2"
-    data["start"] = data.index * 10000
-    data["end"] = data["start"] + 10000
+    data["start"] = data.index * 200
+    data["end"] = data["start"] + 200
+    data["score"] = 1000
+    data["strand"] = "."
+    
+    data = data[data["start"] < chr2_len]
+    data.loc[data["end"] > chr2_len, "end"] = chr2_len
+    
     data["name"] = "state_" + data["state"].astype(str)
-    bed_df = data[["chrom", "start", "end", "name"]]
-    bed_df.to_csv("chr2_5_State_hidddenmm_states.bed", sep="\t", header=False, index=False)
+    bed_df = data[["chrom", "start", "end", "name","score","strand"]]
+    bed_df.to_csv(f"chr2_{n_components}_State_hidddenmm_states.bed", sep="\t", header=False, index=False)
 
 	
 if __name__ == "__main__":
